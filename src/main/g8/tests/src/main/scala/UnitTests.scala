ab5c5e9eae37a1c4a02607a04c8e822fdd7c0cc8
Gerolf Seitz <gerolf.seitz@gmail.com>
2010-10-13 23:51:14 +0200

    again, use "package" paramter

--- src/main/g8/tests/src/main/scala/UnitTests.scala
+++ src/main/g8/tests/src/main/scala/UnitTests.scala
@@ -1,10 +1,10 @@
-package my.package.tests
+package $package$.tests
 
 import junit.framework.Assert._
 import _root_.android.test.AndroidTestCase
 
 class UnitTests extends AndroidTestCase {
   def testPackageIsCorrect {
-    assertEquals("my.package", getContext.getPackageName)
+    assertEquals("$package$", getContext.getPackageName)
   }
 }

0d7bd094ebb1269d8727c2f3c98552c2b1594f8a
Gerolf Seitz <gerolf.seitz@gmail.com>
2010-10-14 01:16:43 +0200

    no need to exclude *.png's anymore. binaries don't work well with stringtemplate anyhow

--- project/build/project.scala
+++ project/build/project.scala
@@ -1,7 +1,3 @@
 import sbt._
 
-class TemplateProject(info: ProjectInfo) extends DefaultProject(info) with giter8.Template {
-  
-  override def templateSources = super.templateSources --- (descendents(templateSourcePath, "*.png"))
-  
-}
+class TemplateProject(info: ProjectInfo) extends DefaultProject(info) with giter8.Template

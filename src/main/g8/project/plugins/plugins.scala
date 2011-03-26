97f36a3aa4097a36c3b58f7bdd389549db6df9b0
Gerolf Seitz <gerolf.seitz@gmail.com>
2011-03-26 23:00:00 +0100

    upgrade sbt-android-plugin to 0.5.1

--- src/main/g8/project/plugins/plugins.scala
+++ src/main/g8/project/plugins/plugins.scala
@@ -1,5 +1,5 @@
 import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  val android = "org.scala-tools.sbt" % "sbt-android-plugin" % "0.5.0"
+  val android = "org.scala-tools.sbt" % "sbt-android-plugin" % "0.5.1"
 }
